6269ec8b5215929a91fb4817824b3b9dbe53040b
Alberto Chávez [GDL] <dchavez@compusoluciones.com>
2021-11-18 18:44:15 -0600

    add ItemCount component

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,51 @@
+import {useState} from 'react';
+import './styles.css';
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [counter, setCounter] = useState(initial);
+
+    const handleIncrementCounter = () => {
+        if (counter < stock) {
+            setCounter(counter + 1)
+        }
+    }
+
+    const handleDecrementCounter = () => {
+        if (counter > 0) {
+            setCounter(counter - 1)
+        }
+    }
+    return (
+        <div className="widthContainer">
+            <div className="quantity buttons_added button_add_container">
+                <div className="row">
+                    <div className="col" align="center">
+                        <button onClick={handleDecrementCounter}
+                            className="minus text-left">-</button>
+                    </div>
+                    <div className="col" align="center">
+                        <input type="number" className="input-text qty text"
+                            value={counter}/>
+
+                    </div>
+                    <div className="col" align="center">
+                        <button type="button" value="+" className="plus"
+                            onClick={handleIncrementCounter}>+</button>
+
+                    </div>
+                </div>
+            </div>
+            <div className="button_add_container">
+                <button className="button_add btn btn-outline-primary"
+                    onClick={
+                        () => onAdd(counter)
+                    }
+                    disabled={
+                        stock === 0
+                }>Agregar al carrito</button>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount
--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { FaBeer } from 'react-icons/fa';
+import React from 'react';
+import ItemCount from '../../components/ItemCount';
 
 const ItemListContainer = ({greeting}) => {
+
+    const onAdd = (counter) => {
+        if (counter === 0) {
+            alert("Debes ingresar al menos un producto")
+        } else {
+            alert(`Has ingresado ${counter} productos`)
+        }
+    }
     return (
         <div>
             <h1>{greeting}</h1>
-
+            <ItemCount stock={5}
+                initial={1}
+                onAdd={onAdd}/>
         </div>
     )
 }
